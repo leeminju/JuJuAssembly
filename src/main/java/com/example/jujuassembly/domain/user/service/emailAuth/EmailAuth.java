c82603a4d805588f57d159f663e02656c7e81c2b
k-jaehyun <wogus146@gmail.com>
2024-01-07 16:59:01 +0900

    fit: EmailAuth 애너테이션 수정

--- src/main/java/com/example/jujuassembly/domain/user/service/emailAuth/EmailAuth.java
+++ src/main/java/com/example/jujuassembly/domain/user/service/emailAuth/EmailAuth.java
@@ -6,12 +6,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "email_auth")
 public class EmailAuth extends Timestamped {
 
   @Id
