6d4175162097ab074d6d6074529733c443a2afbc
leeminju <leemj4090@naver.com>
2024-01-08 17:23:00 +0900

    refactor : Review 작성 리팩토링

--- src/main/java/com/example/jujuassembly/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/jujuassembly/domain/review/controller/ReviewController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
