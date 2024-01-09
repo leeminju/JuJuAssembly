fa070c6c87d78a60264d0663ec0ed44daba91501
yeongdo99 <atpsfka1@gmail.com>
2024-01-09 16:09:10 +0900

    feat : Product Repository CategotyId 기준 정렬 구현

--- src/main/java/com/example/jujuassembly/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/jujuassembly/domain/product/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package com.example.jujuassembly.domain.product.repository;
 
 import com.example.jujuassembly.domain.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // 카테고리 ID를 기준으로 상품 조회.
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
 }
