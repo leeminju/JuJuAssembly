c4664bbb525e6e97c047096de4e8cd53e31e859e
susik2023 <tntlr92@naver.com>
2024-01-09 21:26:34 +0900

    feat: report CRUD 구현

--- src/main/java/com/example/jujuassembly/domain/report/entity/Report.java
+++ src/main/java/com/example/jujuassembly/domain/report/entity/Report.java
@@ -1,16 +1,19 @@
 package com.example.jujuassembly.domain.report.entity;
 
 import com.example.jujuassembly.domain.category.entity.Category;
+import com.example.jujuassembly.domain.report.dto.ReportRequestDto;
 import com.example.jujuassembly.global.entity.Timestamped;
 import com.example.jujuassembly.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 @Table(name = "reports")
 public class Report extends Timestamped {
@@ -37,5 +40,29 @@ public class Report extends Timestamped {
     @JoinColumn(name = "category_id")
     private Category category;
 
+    public Report(ReportRequestDto requestDto) {
+        this.name = requestDto.getName();
+        this.status = StatusEnum.PROCEEDING;
+    }
+
+    public void updateUser(User user) {
+        this.user = user;
+    }
+
+    public void updateCategory(Category category) {
+        this.category = category;
+    }
+
+    public void updateImage(String image) {
+        this.image = image;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void updateStatus(StatusEnum status) {
+        this.status = status;
+    }
 }
 
