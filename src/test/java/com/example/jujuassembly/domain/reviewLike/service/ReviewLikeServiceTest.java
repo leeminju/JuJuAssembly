b98e1616ce67e5d3f6aa933014bade324b45c3b6
leeminju <leemj4090@naver.com>
2024-01-11 00:13:39 +0900

    test : 리뷰 추천/비추천 테스트 구현 및 기존 서비스 코드 수정

--- src/test/java/com/example/jujuassembly/domain/reviewLike/service/ReviewLikeServiceTest.java
+++ src/test/java/com/example/jujuassembly/domain/reviewLike/service/ReviewLikeServiceTest.java
@@ -1,8 +1,9 @@
 package com.example.jujuassembly.domain.reviewLike.service;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -13,6 +14,7 @@ import com.example.jujuassembly.domain.product.entity.Product;
 import com.example.jujuassembly.domain.product.repository.ProductRepository;
 import com.example.jujuassembly.domain.review.entity.Review;
 import com.example.jujuassembly.domain.review.repository.ReviewRepository;
+import com.example.jujuassembly.domain.reviewLike.dto.ReviewLikeResponseDto;
 import com.example.jujuassembly.domain.reviewLike.entity.ReviewLike;
 import com.example.jujuassembly.domain.reviewLike.entity.ReviewLikeStatusEnum;
 import com.example.jujuassembly.domain.reviewLike.repository.ReviewLikeRepository;
@@ -25,6 +27,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -51,6 +54,7 @@ class ReviewLikeServiceTest {
   private Long reviewId = 3L;
   private Long reviewLikeId = 4L;
   private Long userId = 5L;
+  private String loginId = "tester";
   private User user;
   private Product product;
   private Category category;
@@ -60,7 +64,7 @@ class ReviewLikeServiceTest {
   @BeforeEach
   void setUp() {
     // Mock 객체 생성 및 초기화
-    user = User.builder().id(userId).build();
+    user = User.builder().id(userId).loginId(loginId).build();
     category = Category.builder().id(categoryId).build();
     product = Product.builder().id(productId).category(category).build();
     review = Review.builder().id(reviewId).product(product).build();
@@ -74,28 +78,54 @@ class ReviewLikeServiceTest {
   @DisplayName("리뷰 추천 테스트 - 아무 기록 없을 때 ")
   void likeReviewTest() {
     // given
+    ReviewLike mockReviewLike = ReviewLike.builder().id(reviewLikeId).user(user).review(review)
+        .status(ReviewLikeStatusEnum.LIKE)
+        .build();
 
+    when(reviewLikeRepository.findByReviewAndUser(review, user)).thenReturn(
+        Optional.empty());
+
+    when(reviewLikeRepository.save(any(ReviewLike.class))).thenReturn(mockReviewLike);
     // when
-    boolean result = reviewLikeService.likeReview(categoryId, productId, reviewId, user);
+    Optional<ReviewLikeResponseDto> result = reviewLikeService.likeReview(categoryId,
+        productId,
+        reviewId, user);
 
     // then
-    assertTrue(result);
-    verify(reviewLikeRepository, times(1)).save(any(ReviewLike.class));
+    assertTrue(result.isPresent());
+    result.ifPresent(actualValue -> {
+      assertEquals(reviewId, actualValue.getReviewId());
+      assertEquals(reviewLikeId, actualValue.getId());
+      assertEquals(ReviewLikeStatusEnum.LIKE, actualValue.getStatus());
+      assertEquals(loginId, actualValue.getUserLoginId());
+    });
+
   }
 
   @Test
   @DisplayName("리뷰 추천 테스트 - 비추천 기록 있을 때 ")
   void likeReviewTest2() {
     // given
-    ReviewLike reviewLike = ReviewLike.builder().id(reviewLikeId).user(user)
+    ReviewLike mockReviewLike = ReviewLike.builder().id(reviewLikeId).user(user).review(review)
         .status(ReviewLikeStatusEnum.DISLIKE)
         .build();
+
+    when(reviewLikeRepository.findByReviewAndUser(review, user)).thenReturn(
+        Optional.of(mockReviewLike));
+
     // when
-    boolean result = reviewLikeService.likeReview(categoryId, productId, reviewId, user);
+    Optional<ReviewLikeResponseDto> result = reviewLikeService.likeReview(categoryId,
+        productId,
+        reviewId, user);
 
     // then
-    assertTrue(result);
-    verify(reviewLikeRepository, times(1)).save(any(ReviewLike.class));
+    assertTrue(result.isPresent());
+    result.ifPresent(actualValue -> {
+      assertEquals(reviewId, actualValue.getReviewId());
+      assertEquals(reviewLikeId, actualValue.getId());
+      assertEquals(ReviewLikeStatusEnum.LIKE, actualValue.getStatus());
+      assertEquals(loginId, actualValue.getUserLoginId());
+    });
   }
 
   @Test
@@ -110,10 +140,11 @@ class ReviewLikeServiceTest {
         Optional.of(reviewLike));
 
     // when
-    boolean result = reviewLikeService.likeReview(categoryId, productId, reviewId, user);
+    Optional<ReviewLikeResponseDto> result = reviewLikeService.likeReview(categoryId, productId,
+        reviewId, user);
 
     // then
-    assertFalse(result);
+    assertTrue(result.isEmpty());
     verify(reviewLikeRepository, times(1)).delete(reviewLike);
   }
 
@@ -121,29 +152,53 @@ class ReviewLikeServiceTest {
   @DisplayName("리뷰 비추천 테스트 - 아무 기록 없을 떄")
   void dislikeReviewTest() {
     // given
+    ReviewLike mockReviewLike = ReviewLike.builder().id(reviewLikeId).user(user).review(review)
+        .status(ReviewLikeStatusEnum.DISLIKE)
+        .build();
 
+    when(reviewLikeRepository.findByReviewAndUser(review, user)).thenReturn(
+        Optional.empty());
+
+    when(reviewLikeRepository.save(any(ReviewLike.class))).thenReturn(mockReviewLike);
     // when
-    boolean result = reviewLikeService.dislikeReview(categoryId, productId, reviewId, user);
+    Optional<ReviewLikeResponseDto> result = reviewLikeService.dislikeReview(categoryId,
+        productId,
+        reviewId, user);
 
     // then
-    assertTrue(result);
-    verify(reviewLikeRepository, times(1)).save(any(ReviewLike.class));
+    assertTrue(result.isPresent());
+    result.ifPresent(actualValue -> {
+      assertEquals(reviewId, actualValue.getReviewId());
+      assertEquals(reviewLikeId, actualValue.getId());
+      assertEquals(ReviewLikeStatusEnum.DISLIKE, actualValue.getStatus());
+      assertEquals(loginId, actualValue.getUserLoginId());
+    });
   }
 
   @Test
   @DisplayName("리뷰 비추천 테스트 - 추천 기록 있을 떄")
   void dislikeReviewTest2() {
     // given
-    ReviewLike reviewLike = ReviewLike.builder().id(reviewLikeId).user(user)
+    ReviewLike mockReviewLike = ReviewLike.builder().id(reviewLikeId).user(user).review(review)
         .status(ReviewLikeStatusEnum.LIKE)
         .build();
 
+    when(reviewLikeRepository.findByReviewAndUser(review, user)).thenReturn(
+        Optional.of(mockReviewLike));
+
     // when
-    boolean result = reviewLikeService.dislikeReview(categoryId, productId, reviewId, user);
+    Optional<ReviewLikeResponseDto> result = reviewLikeService.dislikeReview(categoryId,
+        productId,
+        reviewId, user);
 
     // then
-    assertTrue(result);
-    verify(reviewLikeRepository, times(1)).save(any(ReviewLike.class));
+    assertTrue(result.isPresent());
+    result.ifPresent(actualValue -> {
+      assertEquals(reviewId, actualValue.getReviewId());
+      assertEquals(reviewLikeId, actualValue.getId());
+      assertEquals(ReviewLikeStatusEnum.DISLIKE, actualValue.getStatus());
+      assertEquals(loginId, actualValue.getUserLoginId());
+    });
   }
 
   @Test
@@ -158,10 +213,11 @@ class ReviewLikeServiceTest {
         Optional.of(reviewLike));
 
     // when
-    boolean result = reviewLikeService.dislikeReview(categoryId, productId, reviewId, user);
+    Optional<ReviewLikeResponseDto> result = reviewLikeService.dislikeReview(categoryId, productId,
+        reviewId, user);
 
     // then
-    assertFalse(result);
+    assertTrue(result.isEmpty());
     verify(reviewLikeRepository, times(1)).delete(reviewLike);
   }
 
