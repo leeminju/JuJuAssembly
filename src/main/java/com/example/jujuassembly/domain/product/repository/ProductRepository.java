5ecf09b010368b93205838855f6534750cf05b47
yeongdo99 <atpsfka1@gmail.com>
2024-01-11 10:51:35 +0900

    refactor : 컨벤션 적용

--- src/main/java/com/example/jujuassembly/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/jujuassembly/domain/product/repository/ProductRepository.java
@@ -8,13 +8,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    // 카테고리 ID를 기준으로 상품 조회.
-    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
 
-    @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
-    Page<Product> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
+  // 카테고리 ID를 기준으로 상품 조회.
+  Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
+
+  @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+  Page<Product> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
