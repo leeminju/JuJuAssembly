1438964e5f43e2b03abba7ee3c85da362ca11ef4
yeongdo99 <atpsfka1@gmail.com>
2024-01-15 13:05:48 +0900

    add : @RequestMapping 추가

--- src/main/java/com/example/jujuassembly/domain/notification/controller/NotificationController.java
+++ src/main/java/com/example/jujuassembly/domain/notification/controller/NotificationController.java
@@ -11,12 +11,14 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1/notification")
 public class NotificationController {
 
   private final NotificationService notificationService;
