0e9e1afcc9c0933d96b0a6a20432fbe5e1abf48d
leeminju <leemj4090@naver.com>
2024-01-15 11:36:09 +0900

    Feature/front (#50) add : 로그인 회원가입 페이지 구현

--- src/main/java/com/example/jujuassembly/global/controller/PageController.java
+++ src/main/java/com/example/jujuassembly/global/controller/PageController.java
@@ -5,10 +5,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+
+  @GetMapping("/")
+  public String home() {
+    return "index";
+  }
+
+  @GetMapping("/login")
+  public String loginPage() {
+    return "login";
+  }
+
+  @GetMapping("/signup")
+  public String signupPage() {
+    return "signup";
+  }
+
+  @GetMapping("/admin")
+  public String adminPage() {
+    return "admin";
+  }
+/*
   @GetMapping("/")
   public String home(){
     return "product";
   }
+*/
 
 
 }
