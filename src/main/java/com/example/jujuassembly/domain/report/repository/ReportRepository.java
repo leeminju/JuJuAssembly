0de9c4aa249a614dfd66a1aad4c3f061c315599a
susik2023 <tntlr92@naver.com>
2024-01-17 19:59:21 +0900

    Test: 전체제보상품조회,카테고리별 제보상품조회 테스트코드 추가.

--- src/main/java/com/example/jujuassembly/domain/report/repository/ReportRepository.java
+++ src/main/java/com/example/jujuassembly/domain/report/repository/ReportRepository.java
@@ -2,7 +2,6 @@ package com.example.jujuassembly.domain.report.repository;
 
 import com.example.jujuassembly.domain.report.entity.Report;
 import com.example.jujuassembly.global.exception.ApiException;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,5 +17,8 @@ public interface ReportRepository extends JpaRepository<Report, Long> {
 
   Page<Report> findAllByUserId(Long userId, Pageable pageable);
 
-  Optional<Report> findByUserId(Long userId);
+  Page<Report> findAllByCategoryId(Long categoryId, Pageable pageable);
+
+  Page<Report> findAll(Pageable pageable);
+
 }
