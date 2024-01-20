55487c4210b446972fb0a204c7a011e783da56e6
yeonwooPark <yeonwoo5115@gmail.com>
2024-01-20 17:16:12 +0900

    fix: userManageFront 수정 (관리자 전체 목록조회 pageable로 수정)

--- src/main/resources/static/js/userManage.js
+++ src/main/resources/static/js/userManage.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  getUsers();
+  getAllUsers();
 })
 
-function getUsers() {
+/*function getUsers(){
   $.ajax({
     type: 'GET'
     , url: `/v1/users`
@@ -45,7 +45,78 @@ function getUsers() {
   
     </p>
   </div>`;
+        $('#userList').append(html);
+      }
+    }, error(error, status, request) {
+      console.log(error);
+    }
+  });
+}*/
 
+function getAllUsers() {
+  var sorting = $("#sorting option:selected").val();
+  var isAsc = $(':radio[name="isAsc"]:checked').val();
+
+  let dataSource = `/v1/users?sort=${sorting},${isAsc}`;
+
+  $('#userList').empty();
+  $('#pagination').pagination({
+    dataSource,
+    locator: 'data.content',
+    alias: {
+      pageNumber: 'page',
+      pageSize: 'size'
+    },
+    totalNumberLocator: (response) => {
+      return response.data.totalElements;//전체 갯수
+    },
+    pageSize: 80,
+    showPrevious: true,
+    showNext: true,
+    ajax: {
+      error(error, status, request) {
+        console.log(error);
+      }
+    },
+    callback: function (response, pagination) {
+      $('#userList').empty();
+      let users = response;
+      for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+        let userId = user['id'];
+        let image = user['image'];
+        let loginId = user['loginId']
+        let nickname = user['nickname']
+        let email = user['email']
+        let role = user['role'];
+        let firstPreferredCategoryId = user['firstPreferredCategoryId'];
+        let secondPreferredCategoryId = user['secondPreferredCategoryId'];
+
+        if (!image) {
+          image = "/images/default_user_image.png"
+        }
+        let html = `<div class="d-flex text-body-secondary pt-3">
+        <img style="margin: 7px; width: 40px;height: 40px;object-fit: contain;" src=${image}>
+        <p class="pb-3 mb-0 small lh-sm border-bottom"  style="display: flex;flex-direction: row;justify-content: center;">
+           <div style="width: 80%">
+             <strong class="d-block text-gray-dark">${loginId}(${nickname})</strong>
+             email : ${email}<br>
+             role : ${role}
+           </div>
+           <div style="width: 22%">
+              <button class="btn btn-light" onclick="location.href='/userReview?userId=${userId}'">리뷰 목록</button>
+               <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+               권한 변경
+               </button>
+                  <ul class="dropdown-menu">
+                     <li onclick="changeRole(${userId}, 'ADMIN')"><a class="dropdown-item" href="#">ADMIN</a></li>
+                     <li onclick="changeRole(${userId}, 'USER')"><a class="dropdown-item" href="#">USER</a></li>
+                    <li onclick="changeRole(${userId}, 'BANNED')"><a class="dropdown-item" href="#">BANNED</a></li>
+                   </ul>
+            </div>
+  
+        </p>
+    </div>`;
         $('#userList').append(html);
       }
     }, error(error, status, request) {
