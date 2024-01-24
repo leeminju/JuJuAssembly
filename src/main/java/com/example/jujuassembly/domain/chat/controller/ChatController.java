b2e7c0ab3f45c546f58c986cabb064ef98bfbfe9
susik2023 <tntlr92@naver.com>
2024-01-24 11:44:56 +0900

    refactor : chatcontroller의 매핑주소 수정,v1추가

--- src/main/java/com/example/jujuassembly/domain/chat/controller/ChatController.java
+++ src/main/java/com/example/jujuassembly/domain/chat/controller/ChatController.java
@@ -10,11 +10,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1/")
 public class ChatController {
 
   private final ChatService chatService;
@@ -25,7 +27,7 @@ public class ChatController {
    * @param userId 사용자 ID
    * @return 최근 채팅 기록과 함께 성공 응답 반환
    */
-  @GetMapping("/messages/{userId}")
+  @GetMapping("/chats/{userId}")
   public ResponseEntity<ApiResponse<List<LatestChatResponseDto>>> findAllLatestChats(
       @PathVariable Long userId) {
     List<LatestChatResponseDto> latestChatResponseDtoList = chatService.findAllLatestChats(userId);
@@ -40,7 +42,7 @@ public class ChatController {
    * @param roomId 채팅방 ID
    * @return 채팅방의 모든 채팅 기록과 함께 성공 응답 반환
    */
-  @GetMapping("/messages")
+  @GetMapping("/chats")
   public ResponseEntity<ApiResponse<List<ChatResponseDto>>> findAllChats(
       @RequestParam("roomId") Long roomId) {
     List<ChatResponseDto> chatResponseDtoList = chatService.findAllChats(roomId);
