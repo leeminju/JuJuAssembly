82be5b2bb2b282e5080e81e1b6be4ea0c71d512e
leeminju <leemj4090@naver.com>
2024-01-25 20:10:15 +0900

    fix : 안쓰는 메소드 삭제

--- src/main/java/com/example/jujuassembly/domain/chat/repository/ChatRepository.java
+++ src/main/java/com/example/jujuassembly/domain/chat/repository/ChatRepository.java
@@ -7,11 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-
-  default Chat findChatByIdOrElseThrow(Long id) {
-    return findById(id).orElseThrow(
-        () -> new ApiException("해당 채팅을 찾을 수 없습니다.", HttpStatus.NOT_FOUND)
-    );
-  }
-
 }
